a686892fce09922ccedeb82ea73ac4ad5baf1f77
Edward Stuart Johnson <edwardstuartjohnson0@gmail.com>
2023-11-25 22:41:10 +0000

    Add milestone_5.py with game's logic.

--- milestone_5.py
+++ milestone_5.py
@@ -12,16 +12,17 @@ class Hangman():
         Good guess! a is in the word.
 
         """
-        while True:
-            guess = input("I am thinking of a word. Please guess a letter in the word: ") # asks user to guess a letter
+        # while True:
+        print(f"You have {str(self.num_lives)} lives left. The number of letters you need to guess now is {str(self.num_letters)}")
+        guess = input("I am thinking of a word. Please guess a letter in the word: ") # asks user to guess a letter
 
-            if len(guess) > 1 or guess.isalpha() == False: # checks if the length of the input is equal to 1 and the input is alphabetical:
-                print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
-            else:
-                self._check_guess(guess)
-                self.list_of_guesses.append(guess) # append the guess to the list_of_guesses
+        if len(guess) > 1 or guess.isalpha() == False: # checks if the length of the input is equal to 1 and the input is alphabetical:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+        elif guess in self.list_of_guesses:
+            print("You already tried that letter!")
+        else:
+            self._check_guess(guess)
+            self.list_of_guesses.append(guess) # append the guess to the list_of_guesses
 
     def _check_guess(self, guess):
         """
@@ -40,12 +41,31 @@ class Hangman():
 
     def _insert_correctly_guessed_letter(self, guess):
         """
-        This method looks throguh each letter in the word, finds where the correctl guessed letter is, and then replaces the relevant underscore (_) or underscores plural in the word_guessed with the correctly-guessed letter. 
+        This method looks through each letter in the word, finds where the correctl guessed letter is, and then replaces the relevant underscore (_) or underscores plural in the word_guessed with the correctly-guessed letter. 
 
         """
         for letter_position in range(len(self.word)): # go through each position in the word to check if an "_" must be replaced by the successfully guessed letter
             if self.word[letter_position] == guess:
                 self.word_guessed[letter_position] = guess
+    
+    def play_game(word_list):
+        """
+        This method allows you to play the game: it creates an instance of the Hangman class and includes the logic for the game.
+        """
+        num_lives = 5
+
+        game = Hangman(word_list, num_lives)
+
+        while True:
+            if game.num_lives == 0:
+                print("You lost!")
+                break
+            elif game.num_letters > 0:
+                game._ask_for_input()
+            else:
+                print("Congratulations. You won the game!")
+                break
+
 
     def __init__(self, word_list, num_lives=5):
         """
@@ -58,16 +78,16 @@ class Hangman():
         self.word = random.choice(self.word_list) #The word to be guessed, picked randomly from the word_list. Remember to import the random module into your script
         self.word_guessed = list(["_"] * len(self.word)) # A list of the letters of the word, with _ for each letter not yet guessed. For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']. If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
         self.num_letters = int(len(set(self.word))) # The number of UNIQUE letters in the word that have not been guessed yet
+        print("The number of UNIQUE letters in the word is " + str(self.num_letters))
         self.num_lives = int(num_lives) # The number of lives the player has at the start of the game.
         self.list_of_guesses = list() #[,] # A list of the guesses that have already been tried. Set this to an empty list initially
-        self._ask_for_input()
-
+        
 
 if __name__ == '__main__':
 
-    word_list = ["banana"] #,"tomato", "apple",  "orange", "mango"]
+    word_list = ["banana","tomato", "apple",  "orange", "mango"]
 
     print(word_list)
 
-    current_game = Hangman(word_list)
+    Hangman.play_game(word_list)
 
